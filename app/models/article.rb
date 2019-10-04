a447851c94951e2b108c5e325b76cb7bc69efefb
R,Manohar <Manohar.R@cerner.com>
2019-10-04 12:39:49 +0530

    edit,delete,search added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 
 class Article < ApplicationRecord
+	validates :title, presence: true
+                 
+    validates :author, presence: true
+                    
+    validates :text, length: { minimum: 10 }
 end
