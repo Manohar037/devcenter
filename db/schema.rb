f883e304d38d8e6ce8d60eda6951c84ac268bce4
R,Manohar <Manohar.R@cerner.com>
2019-10-17 18:24:27 +0530

    devise gem added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_150055) do
+ActiveRecord::Schema.define(version: 2019_10_17_123354) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2019_10_09_150055) do
     t.string "author_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_authors_on_email"
+    t.index ["reset_password_token"], name: "index_authors_on_reset_password_token"
   end
 
   add_foreign_key "articles", "authors"
